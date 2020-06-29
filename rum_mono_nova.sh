90bf4413e8622a58c94bcc905d039fca3b340075
pangfumin <pangfumin@gmail.com>
2020-06-29 11:49:43 +0800

    run mono nova

---
+++ rum_mono_nova.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular/mono_my_nova /home/pang/software/ORB_SLAM2/Vocabulary/ORBvoc.bin  /home/pang/software/ORB_SLAM2/Examples/Monocular/huawei_nova5.yaml /home/pang/dataset/ar/test_desk
+
---
+++ rum_tum_ar.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular/mono_tum_ar /home/pang/software/ORB_SLAM2/Vocabulary/ORBvoc.bin  /home/pang/software/ORB_SLAM2/Examples/Monocular/TUM2.yaml /home/pang/dataset/tum/rgbd_dataset_freiburg2_desk
+
