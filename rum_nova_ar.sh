193a09c90868b33fe181924e9e5d51e3563ba266
pangfumin <pangfumin@gmail.com>
2020-06-29 12:02:33 +0800

    add mono_nova_ar

---
+++ rum_nova_ar.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular/mono_nova_ar /home/pang/software/ORB_SLAM2/Vocabulary/ORBvoc.bin  /home/pang/software/ORB_SLAM2/Examples/Monocular/huawei_nova5.yaml /home/pang/dataset/ar/test_desk
+
