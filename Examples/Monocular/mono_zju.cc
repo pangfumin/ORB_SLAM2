904174d5dd2a52d0189d9581e4624e9692850fba
pangfumin <pangfumin@gmail.com>
2020-07-08 21:06:19 +0800

    fxi

---
+++ Examples/Monocular/mono_zju.cc
@@ -0,0 +1,154 @@
+/**
+* This file is part of ORB-SLAM2.
+*
+* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/raulmur/ORB_SLAM2>
+*
+* ORB-SLAM2 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM2 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+
+#include<System.h>
+#include "../ROS/ORB_SLAM2/src/AR/FileSystemTools.h"
+#include <Time.hpp>
+
+using namespace std;
+
+void LoadImages(const string &dataset_folder, vector<string> &vstrImageFilenames,
+                vector<Time> &vTimestamps);
+
+int main(int argc, char **argv)
+{
+    if(argc != 4)
+    {
+        cerr << endl << "Usage: ./mono_tum path_to_vocabulary path_to_settings path_to_sequence" << endl;
+        return 1;
+    }
+
+    // Retrieve paths to images
+    vector<string> vstrImageFilenames;
+    vector<Time> vTimestamps;
+    string strFile = string(argv[3])+"/camera/images";
+    LoadImages(strFile, vstrImageFilenames, vTimestamps);
+
+    int nImages = vstrImageFilenames.size();
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
+
+    // Vector for tracking time statistics
+    vector<Time> vTimesTrack;
+    vTimesTrack.resize(nImages);
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing sequence ..." << endl;
+    cout << "Images in the sequence: " << nImages << endl << endl;
+
+    // Main loop
+    cv::Mat im;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        // Read image from file
+        im = cv::imread(vstrImageFilenames[ni],CV_LOAD_IMAGE_GRAYSCALE);
+        double tframe = vTimestamps[ni].toSec();
+
+        if(im.empty())
+        {
+            cerr << endl << "Failed to load image at: "
+                 << string(argv[3]) << "/" << vstrImageFilenames[ni] << endl;
+            return 1;
+        }
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+        // Pass the image to the SLAM system
+        SLAM.TrackMonocular(im,tframe);
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+//        vTimesTrack[ni]=ttrack;
+
+        // Wait to load the next frame
+        double T=0;
+        if(ni<nImages-1)
+            T = 0.033 * (ni+1)-tframe;
+        else if(ni>0)
+            T = tframe- 0.033 * (ni-1);
+
+        if(ttrack<T)
+            usleep((T-ttrack)*1e6);
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Tracking time statistics
+    sort(vTimesTrack.begin(),vTimesTrack.end());
+    float totaltime = 0;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        totaltime+=(float)vTimesTrack[ni].toSec();
+    }
+    cout << "-------" << endl << endl;
+    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
+    cout << "mean tracking time: " << totaltime/nImages << endl;
+
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+    return 0;
+}
+void LoadImages(const std::string &dataset_folder, std::vector<std::string> &vstrImageFilenames, std::vector<Time> &vTimestamps)
+{
+    common::getAllFilesInFolder(dataset_folder, &vstrImageFilenames);
+    std::cout<<"image0_list: " << vstrImageFilenames.size() << std::endl;
+
+    std::sort(vstrImageFilenames.begin(),vstrImageFilenames.end(), [](std::string a, std::string b) {
+        return !common::compareNumericPartsOfStrings(a,b);
+    });
+
+    for (auto  name: vstrImageFilenames) {
+        std::cout << "name: " << name << std::endl;
+        std::string path, file_name, file_name_ext, ext;
+        common::splitPathAndFilename(name, &path, &file_name_ext);
+        common::splitFilePathAndExtension(file_name_ext, &file_name, &ext);
+        std::cout << file_name << std::endl;
+        uint64_t  nsec = std::stol(file_name);
+
+        Time ts; ts.fromNSec(nsec);
+
+        vTimestamps.push_back(ts);
+
+
+
+    }
+
+}
---
+++ rum_mono_zju.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular/mono_zju /home/pang/software/ORB_SLAM2/Vocabulary/ORBvoc.bin  /home/pang/software/ORB_SLAM2/Examples/Monocular/zju.yaml /home/pang/dataset/zjucvg/A7
+
